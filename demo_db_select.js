eb39ad03a6ac82f44b54841d1030dc37b511a9b4
Rodney <rodsan.724@outlook.com>
2018-06-09 13:48:55 -0400

    chore

--- demo_db_select.js
+++ demo_db_select.js
@@ -2,17 +2,17 @@ var mysql = require('mysql');
 require('dotenv/config')
 
 var con = mysql.createConnection({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  con.query("SELECT * FROM " + "fieDb" + ".Contracts", function (err, result, fields) {
+con.connect(function (err) {
     if (err) throw err;
-    console.log(result);
-    con.end();
-  });
+    con.query("SELECT * FROM " + "fieDb" + ".Contracts", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+        con.end();
+    });
 });
